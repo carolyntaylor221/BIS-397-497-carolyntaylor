95daecaa8813a136772520fe17f2cfcb2fc63622
Carolyn Taylor <cnt221@lehigh.edu>
2020-05-26 13:46:31 -0400

    Added excersize 2.8

---
+++ Assignment 2 - Exercise 2.8.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 25 16:22:01 2020
+
+@author: carolyntaylor
+"""
+
+
+print("number\tsquare\tcube","\n",0,"\t",0**2,"\t",0**3,"\n",1,"\t",1**2,\
+      "\t",1**3,"\n",2,"\t",2**2,"\t",2**3,"\n",3,"\t",3**2,"\t",3**3,\
+          "\n",4,"\t",4**2,"\t",4**3,"\n",5,"\t",5**2,"\t",5**3)
+    
+
+print("number\tsquare\tcube","\n","{:>4}".format(0),"\t",\
+      "{:>4}".format(0**2),"\t","{:>4}".format(0**3),"\n","{:>4}".format(1),\
+          "\t","{:>4}".format(1**2),"\t","{:>4}".format(1**3),\
+              "\n","{:>4}".format(2),"\t","{:>4}".format(2**2),\
+                  "\t","{:>4}".format(2**3),"\n","{:>4}".format(3),"\t",\
+                      "{:>4}".format(3**2),"\t","{:>4}".format(3**3),"\n",\
+                          "{:>4}".format(4),"\t","{:>4}".format(4**2),"\t",\
+                              "{:>4}".format(4**3),"\n","{:>4}".format(5),\
+                                  "\t","{:>4}".format(5**2),"\t",\
+                                      "{:>4}".format(5**3))
+    
